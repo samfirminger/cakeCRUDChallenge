5aea178c36afb1ce6dc65a69cb832215865b517b
Sam Firminger <Sam.Firminger@dyson.com>
2020-12-09 08:45:37 +0000

    Added box shadow

--- client/src/components/CakeList.js
+++ client/src/components/CakeList.js
@@ -35,6 +35,11 @@ const CakeImage = styled.img`
     width: 250px;
     height: 250px;
     border-radius: 8px;
+    box-shadow: 0 1px 1px rgba(0,0,0,0.12), 
+              0 2px 2px rgba(0,0,0,0.12), 
+              0 4px 4px rgba(0,0,0,0.12), 
+              0 8px 8px rgba(0,0,0,0.12),
+              0 16px 16px rgba(0,0,0,0.12);
 `;
 
 const CakeName = styled.p`
